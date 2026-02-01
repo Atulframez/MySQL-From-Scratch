cd496ac6178b3c0f45571f4b29c4900fd42c96c3
Atulanand09 <atulanand092004@gmail.com>
2026-02-01 22:21:39 +0530

    ADD-Day04

---
+++ Day04.sql
@@ -0,0 +1,10 @@
+CREATE TABLE City (
+    city_id INT PRIMARY KEY,
+    city_name VARCHAR(50),
+    state VARCHAR(50)
+);
+
+INSERT INTO City VALUES
+(1, 'Delhi', 'Delhi'),
+(2, 'Mumbai', 'Maharashtra'),
+(3, 'Noida', 'Uttar Pradesh');
