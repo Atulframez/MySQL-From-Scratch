577f3085682b697319be2aeed375ee9e8bc5b1a4
Atulanand09 <atulanand092004@gmail.com>
2026-02-02 21:34:52 +0530

    Start-learning

---
+++ Day05.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Department (
+    department_id INT PRIMARY KEY,
+    department_name VARCHAR(50)
+);
+
+INSERT INTO Department VALUES
+(1, 'HR'),
+(2, 'Engineering'),
+(3, 'Marketing');
