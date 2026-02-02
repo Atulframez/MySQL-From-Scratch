fd7718c70598dd227836e5bd44ac665184e4ef3c
Atulanand09 <atulanand092004@gmail.com>
2026-02-02 21:35:39 +0530

    Add-table

--- Day05.sql
+++ Day05.sql
@@ -7,3 +7,11 @@ INSERT INTO Department VALUES
 (1, 'HR'),
 (2, 'Engineering'),
 (3, 'Marketing');
+
+CREATE TABLE Employee (
+    employee_id INT PRIMARY KEY,
+    employee_name VARCHAR(50),
+    age INT,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES Department(department_id)
+);
