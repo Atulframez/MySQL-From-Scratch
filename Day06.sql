d53137c2308c8b3e78c7b81bcd129063138ff3de
Atulanand09 <atulanand092004@gmail.com>
2026-02-03 21:31:31 +0530

    Add

--- Day06.sql
+++ Day06.sql
@@ -18,3 +18,20 @@ REFERENCES parent_table(primary_key_column);
 );
 
 SELECT * FROM Person01;
+
+CREATE TABLE City01 (
+  city_id INT PRIMARY KEY,
+  city_name VARCHAR(50)
+) ENGINE=InnoDB;
+
+CREATE TABLE Person (
+  person_id INT PRIMARY KEY,
+  person_name VARCHAR(50),
+  city_id INT,
+  FOREIGN KEY (city_id) REFERENCES City(city_id)
+) ENGINE=InnoDB;
+
+INSERT INTO City VALUES
+(1, 'New York'),
+(2, 'Los Angeles'),
+(3, 'Chicago');
