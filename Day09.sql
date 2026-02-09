f289899ca47282b2898e562fd0877b6037b7f784
Atulanand09 <atulanand092004@gmail.com>
2026-02-09 23:01:40 +0530

    Add

--- Day09.sql
+++ Day09.sql
@@ -4,3 +4,13 @@ CREATE TABLE Brand (
     country VARCHAR(30)
 ) ENGINE=InnoDB;
 
+CREATE TABLE TV (
+    tv_id INT PRIMARY KEY,
+    model VARCHAR(50),
+    screen_size INT,          -- inches
+    resolution VARCHAR(20),
+    price DECIMAL(10,2),
+    brand_id INT,
+    CONSTRAINT fk_tv_brand
+        FOREIGN KEY (brand_id) REFERENCES Brand(brand_id)
+) ENGINE=InnoDB;
