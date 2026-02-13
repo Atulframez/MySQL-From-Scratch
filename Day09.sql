3c5e70ccf61184e3c09f3b5f4995b9b2a7b1c5e7
Atulanand09 <atulanand092004@gmail.com>
2026-02-13 22:06:33 +0530

    Add-file

--- Day09.sql
+++ Day09.sql
@@ -60,3 +60,11 @@ SELECT
 FROM Brand b
 LEFT JOIN TV t ON b.brand_id = t.brand_id
 LEFT JOIN Laptop l ON b.brand_id = l.brand_id;
+
+
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(50) NOT NULL,
+    phone VARCHAR(15),
+    city VARCHAR(30)
+) ENGINE=InnoDB;
