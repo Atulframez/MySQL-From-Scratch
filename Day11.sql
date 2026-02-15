64c36eeb6a7e21a25f19ddba7b7429882a0b47bb
Atulanand09 <atulanand092004@gmail.com>
2026-02-15 22:02:01 +0530

    sab-add

--- Day11.sql
+++ Day11.sql
@@ -15,14 +15,7 @@ CREATE TABLE Menu (
 ) ENGINE=InnoDB;
 
 
-CREATE TABLE Orders (
-    order_id INT PRIMARY KEY,
-    customer_id INT,
-    order_date DATE,
-    total_amount DECIMAL(8,2),
-    CONSTRAINT fk_order_customer
-        FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
-) ENGINE=InnoDB;
+
 
 
 CREATE TABLE OrderDetails (
