adfad09c02a52ce5bb3467de8a8997b04f082c4c
Atulanand09 <atulanand092004@gmail.com>
2026-02-16 19:26:38 +0530

    change-files-and-table

--- Day11.sql
+++ Day11.sql
@@ -15,6 +15,12 @@ CREATE TABLE Menu (
 ) ENGINE=InnoDB;
 
 
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(50) NOT NULL,
+    phone VARCHAR(15),
+    city VARCHAR(30)
+) ENGINE=InnoDB;
 
 
 
