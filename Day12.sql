a1f5798e3560076828a31c0f4bf1baf94484c642
Atulanand09 <atulanand092004@gmail.com>
2026-02-19 20:42:56 +0530

    Add

--- Day12.sql
+++ Day12.sql
@@ -16,3 +16,4 @@ SELECT MIN(salary) AS Minimum_Salary FROM Employee;
 SELECT MAX(salary) AS Maximum_Salary FROM Employee;
 
 SELECT COUNT(*) AS Total_Employees FROM Employee;
+SELECT SUM(salary) AS Total_Salary FROM Employee;
