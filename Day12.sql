b57f3aec1e8a493debe0a9a34f3179790c55cf46
Atulanand09 <atulanand092004@gmail.com>
2026-02-21 18:37:22 +0530

    Add

--- Day12.sql
+++ Day12.sql
@@ -19,7 +19,8 @@ SELECT MAX(salary) AS Maximum_Salary FROM Employee;
 SELECT COUNT(*) AS Total_Employees FROM Employee;
 SELECT SUM(salary) AS Total_Salary FROM Employee;
 
-
+SELECT * FROM Employee
+ORDER BY salary ASC;
 
 SELECT * FROM Employee
 ORDER BY salary DESC;
