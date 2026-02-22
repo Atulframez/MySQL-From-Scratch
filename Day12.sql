cc20e0334a2520fe323903529163b16fd766c2a2
Atulanand09 <atulanand092004@gmail.com>
2026-02-22 12:04:11 +0530

    Add-table

--- Day12.sql
+++ Day12.sql
@@ -22,8 +22,7 @@ SELECT SUM(salary) AS Total_Salary FROM Employee;
 SELECT * FROM Employee
 ORDER BY salary ASC;
 
-SELECT * FROM Employee
-ORDER BY salary DESC;
+
 
 SELECT * FROM Employee
 ORDER BY emp_name ASC;
