39260d63c696ec8403fb089acef596418c36c5c6
Atulanand09 <atulanand092004@gmail.com>
2026-03-27 19:59:10 +0530

    Add-files-table

--- Day13.sql
+++ Day13.sql
@@ -17,7 +17,13 @@ COMMIT;
 
 START TRANSACTION;
 
+SELECT balance FROM account WHERE id = 1;
 
+UPDATE account 
+SET balance = balance - 40 
+WHERE id = 1;  -- write(a) = 20
+
+COMMIT;
 
 
 START TRANSACTION;
