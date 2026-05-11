b2fb6185abaa49c0ab94b1fcf0b1a9a06a853a79
Atul Anand <atulanand092004@gmail.com>
2026-05-11 23:22:49 +0530

    Add-code-file

--- Day11.sql
+++ Day11.sql
@@ -16,12 +16,7 @@ CREATE TABLE Menu (
 
 
 
-CREATE TABLE Customers (
-    customer_id INT PRIMARY KEY,
-    customer_name VARCHAR(50) NOT NULL,
-    phone VARCHAR(15),
-    city VARCHAR(30)
-) ENGINE=InnoDB;
+
 
 
 
